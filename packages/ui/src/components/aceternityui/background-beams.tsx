b205f73911ae09c5b4bceda929621ec76c43caaa
Pritam12F <pritam.das.santu@gmail.com>
2024-12-31 15:06:15 +0530

    fixed an import

--- packages/ui/src/components/aceternityui/background-beams.tsx
+++ packages/ui/src/components/aceternityui/background-beams.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "@workspace/ui/lib/utils.ts";
+import { cn } from "@workspace/ui/lib/utils.js";
 import { motion, AnimatePresence } from "framer-motion";
 import React, { useRef, useState, useEffect } from "react";
 
