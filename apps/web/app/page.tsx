69eb2deef71d9016ac0bfe9e0d76814505bc6390
Pritam12F <pritam.das.santu@gmail.com>
2024-12-31 20:49:52 +0530

    fixed error with some libs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import { Landing } from "@/components/landing";
 import { Navbar } from "@/components/navbar";
 
-export const runtime = "edge";
-
 export default function Page() {
   return (
     <main>
