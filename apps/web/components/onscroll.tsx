2e6aca9d384053f89dbb0dbd2052c7be7be114fa
Pritam12F <pritam.das.santu@gmail.com>
2025-01-01 20:17:52 +0530

    converted onscroll to server comp

--- apps/web/components/onscroll.tsx
+++ apps/web/components/onscroll.tsx
@@ -1,47 +1,22 @@
-"use client";
-
 import db from "@workspace/db";
-import { useEffect, useState } from "react";
 import { Card } from "./onscrollcard";
 
-export function OnScroll() {
-  const [images, setImages] = useState<{ feature: string; url?: string }[]>([]);
+export async function OnScroll() {
+  try {
+    const fetchedImages = await db.query.images.findMany({
+      columns: {
+        url: true,
+        name: true,
+      },
+    });
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const fetchedImages = await db.query.images.findMany({
-          columns: {
-            url: true,
-          },
-        });
+    return fetchedImages.map((feature, idx) => {
+      if (feature.name === "logo") return null;
 
-        const updatedFeatures = features.map((feature, idx1) => {
-          const matchedFeature = fetchedImages.find((_, idx2) => idx1 === idx2);
-          return {
-            ...feature,
-            url: matchedFeature?.url,
-          };
-        });
-
-        setImages(updatedFeatures);
-      } catch (err) {
-        console.error("Error fetching images:", err);
-      }
-    };
-
-    fetchImages();
-  }, []);
-
-  return images.map((image, idx) => (
-    <Card feature={image.feature} url={image.url!} key={idx} />
-  ));
+      return <Card feature={feature.name!} url={feature.url} key={idx} />;
+    });
+  } catch (error) {
+    console.error("Failed to fetch images:", error);
+    return null;
+  }
 }
-
-const features: { feature: string; url?: string }[] = [
-  { feature: "Rich Text Editor" },
-  { feature: "Team Workspaces" },
-  { feature: "Real-Time Collaboration" },
-  { feature: "Organized Notes & Projects" },
-  { feature: "Responsive for different devices" },
-];
