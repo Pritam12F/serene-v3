6a93e0933bf2ac1292c6fba66ed0d4a2843b60a5
Pritam Das <pritam.das.santu@gmail.com>
2025-02-09 14:35:47 +0530

    change db schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -13,7 +13,7 @@ import {
 
 // Users Table
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
+  id: uuid("id").primaryKey(),
   name: text("name").notNull(),
   profilePic: text("profile_pic"),
   email: text("email").unique().notNull(),
