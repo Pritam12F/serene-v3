4212e5e5debce4bcb1104a33929216c864a13c57
Pritam Das <pritam.das.santu@gmail.com>
2025-02-28 17:38:10 +0530

    added suspense for checking loading state

--- apps/web/components/sidebar-extension.tsx
+++ apps/web/components/sidebar-extension.tsx
@@ -14,6 +14,7 @@ import dynamic from "next/dynamic";
 import { useWorkspaces } from "@/hooks/use-workspaces";
 import { useUser } from "@clerk/nextjs";
 import Loading from "@/app/(main)/documents/[[...slug]]/loading";
+import { Suspense } from "react";
 
 export const SidebarExtension = ({
   children,
@@ -36,47 +37,49 @@ export const SidebarExtension = ({
   }
 
   return (
-    <SidebarInset className="bg-white h-screen overflow-y-hidden dark:bg-[#282b32]">
-      <header className="flex h-12 shrink-0 items-center gap-2">
-        <div className="flex flex-1 items-center gap-2 px-3">
-          <SidebarTrigger />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              {postList?.map((post, index) => {
-                if (index === postList.length - 1) {
+    <Suspense fallback={<Loading />}>
+      <SidebarInset className="bg-white h-screen overflow-y-hidden dark:bg-[#282b32]">
+        <header className="flex h-12 shrink-0 items-center gap-2">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {postList?.map((post, index) => {
+                  if (index === postList.length - 1) {
+                    return (
+                      <BreadcrumbItem key={index}>
+                        <BreadcrumbPage className="line-clamp-1">
+                          {post?.name}
+                        </BreadcrumbPage>
+                      </BreadcrumbItem>
+                    );
+                  }
+
                   return (
                     <BreadcrumbItem key={index}>
-                      <BreadcrumbPage className="line-clamp-1">
+                      <BreadcrumbLink className="line-clamp-1">
                         {post?.name}
-                      </BreadcrumbPage>
+                      </BreadcrumbLink>
                     </BreadcrumbItem>
                   );
-                }
-
-                return (
-                  <BreadcrumbItem key={index}>
-                    <BreadcrumbLink className="line-clamp-1">
-                      {post?.name}
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                );
-              })}
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-        <div className="ml-auto px-3">
-          <NavActions />
-        </div>
-      </header>
-      <Editor
-        editable={true}
-        postId={postList ? postList[postList.length - 1]?.id : null}
-        initialContent={
-          postList ? postList[postList.length - 1]?.content : null
-        }
-        title={postList ? postList[postList.length - 1]?.name : "Untitled"}
-      />
-    </SidebarInset>
+                })}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="ml-auto px-3">
+            <NavActions />
+          </div>
+        </header>
+        <Editor
+          editable={true}
+          postId={postList ? postList[postList.length - 1]?.id : null}
+          initialContent={
+            postList ? postList[postList.length - 1]?.content : null
+          }
+          title={postList ? postList[postList.length - 1]?.name : "Untitled"}
+        />
+      </SidebarInset>
+    </Suspense>
   );
 };
